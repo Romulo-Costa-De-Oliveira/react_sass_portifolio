5f35aa8a097ccc3990d74225b6eaadc8e09405e7
Romulo Costa <romulocoliveirag@gmail.com>
2022-08-02 12:20:40 -0300

    Valid information add

--- src/components/ProjectsContainer/index.jsx
+++ src/components/ProjectsContainer/index.jsx
@@ -1,145 +1,39 @@
 import React from "react";
-import {
-  DiHtml5,
-  DiCss3,
-  DiJsBadge,
-  DiNodejsSmall,
-  DiMysql,
-  DiReact,
-} from "react-icons/di";
+import ProjectList from "../../utils/projectsAndIcons";
 import "../../styles/components/projectscontainer.sass";
 
-const projects = [
-  { id: "html", name: "HTML5", icon: <DiHtml5 /> },
-  { id: "css", name: "CSS3", icon: <DiCss3 /> },
-  { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
-  { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
-  { id: "mysql", name: "MySQL", icon: <DiMysql /> },
-  { id: "react", name: "React", icon: <DiReact /> },
-];
+const projects = ProjectList;
 
 const ProjectsContainer = () => {
   return (
     <div id="projects-content">
-      {/* <section className="technologies-container">
+      <section className="projects-container">
         <h2>Projetos</h2>
-        <div className="technologies-grid">
-          {projects.map((tech) => (
-            <div className="technology-card" id={tech.id} key={tech.id}>
-              {tech.icon}
-              <div className="technology-info">
-                <h3>{tech.name}</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        <div className="projects-grid">
+          {projects.map((project) => (
+            <div className="project-card" id={project.id} key={project.id}>
+              <div className="project-info">
+                <h3>{project.name}</h3>
+                <p>{project.descripition}</p>
+                <small>Status: {project.status}</small>
+              </div>
+
+              <div className="tech-info-view">
+                <div className="tech" key="tech">
+                  {project.techicons.map((tech) => (
+                    <div className="techicon" key={tech.id}>
+                      {tech}
+                    </div>
+                  ))}
+                </div>
+                <a className="view-btn" href={project.link}>
+                  {project.type}
+                </a>
               </div>
             </div>
           ))}
         </div>
-      </section> */}
-      <section className="projects-container">
-        <h2>Projetos</h2>
-        <div className="projects-grid">
-          <div className="project-card">
-            <div className="project-info">
-              <h3>nome</h3>
-              <p>
-                Lorem Lorem ipsum dolor sit amet consectetur, adipisicing
-                elit.ipsum dolor sit amet consectetur, adipisicing elit.Lorem
-                ipsum dolor sit amet consectetur, adipisicing elit.V
-              </p>
-            </div>
-            <div className="tech-info-view">
-              <div className="tech">
-                <DiHtml5 color="#E44D26" />
-                <DiNodejsSmall />
-                <DiReact />
-              </div>
-              
-              <a className="view-btn" href="">baixar</a>
-            </div>
-          </div>
-
-          <div className="project-card">
-            <div className="project-info">
-              <h3>nome</h3>
-              <p>
-                Lorem Lorem ipsum dolor sit amet consectetur, adipisicing
-                elit.ipsum dolor sit amet consectetur, adipisicing elit.Lorem
-                ipsum dolor sit amet consectetur, adipisicing elit.V
-              </p>
-            </div>
-            <div className="tech-info-view">
-              <div className="tech">
-                <DiHtml5 color="#E44D26" />
-                <DiNodejsSmall />
-                <DiReact />
-              </div>
-              
-              <a className="view-btn" href="">baixar</a>
-            </div>
-          </div>
-
-          <div className="project-card">
-            <div className="project-info">
-              <h3>nome</h3>
-              <p>
-                Lorem Lorem ipsum dolor sit amet consectetur, adipisicing
-                elit.ipsum dolor sit amet consectetur, adipisicing elit.Lorem
-                ipsum dolor sit amet consectetur, adipisicing elit.V
-              </p>
-            </div>
-            <div className="tech-info-view">
-              <div className="tech">
-                <DiHtml5 color="#E44D26" />
-                <DiNodejsSmall />
-                <DiReact />
-              </div>
-              
-              <a className="view-btn" href="">baixar</a>
-            </div>
-          </div>
-
-          <div className="project-card">
-            <div className="project-info">
-              <h3>nome</h3>
-              <p>
-                Lorem Lorem ipsum dolor sit amet consectetur, adipisicing
-                elit.ipsum dolor sit amet consectetur, adipisicing elit.Lorem
-                ipsum dolor sit amet consectetur, adipisicing elit.V
-              </p>
-            </div>
-            <div className="tech-info-view">
-              <div className="tech">
-                <DiHtml5 color="#E44D26" />
-                <DiNodejsSmall />
-                <DiReact />
-              </div>
-              
-              <a className="view-btn" href="">baixar</a>
-            </div>
-          </div>
-
-          <div className="project-card">
-            <div className="project-info">
-              <h3>nome</h3>
-              <p>
-                Lorem Lorem ipsum dolor sit amet consectetur, adipisicing
-                elit.ipsum dolor sit amet consectetur, adipisicing elit.Lorem
-                ipsum dolor sit amet consectetur, adipisicing elit.V
-              </p>
-            </div>
-            <div className="tech-info-view">
-              <div className="tech">
-                <DiHtml5 color="#E44D26" />
-                <DiNodejsSmall />
-                <DiReact />
-              </div>
-              
-              <a className="view-btn" href="">baixar</a>
-            </div>
-          </div>
-        </div>
       </section>
-      
     </div>
   );
 };
---
+++ src/utils/projectsAndIcons.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  DiJava,
+  DiHtml5,
+  DiCss3,
+  DiJavascript1,
+  DiNodejsSmall,
+  DiMysql,
+  DiReact,
+  DiMongodb,
+  DiBootstrap,
+  DiFirebase,
+} from "react-icons/di";
+
+const i = [
+  <DiJava id="java" color="#339999" />,
+  <DiMongodb id="mongo" color="#3FA037" />,
+  <DiBootstrap id="bootstrap" color="#563d7c" />,
+  <DiFirebase id="firebase" color="#FF9800" />,
+  <DiHtml5 id="html" color="#E44D26" />,
+  <DiCss3 id="css" color="#1572B6" />,
+  <DiJavascript1 id="javascript" color="#F0DB4F" />,
+  <DiNodejsSmall id="nodejs" color="#83CD29" />,
+  <DiMysql id="mysql" color="#00618A" />,
+  <DiReact id="react" color="#61DAFB" />,
+];
+const ProjectList = [
+  {
+    id: "dsmeta",
+    name: "DsMeta",
+    type: "Link",
+    link: "https://github.com/Romulo-Costa-De-Oliveira/dsmeta",
+    techicons: [i[9], i[5], i[4], i[6], i[8], i[0]],
+    descripition:
+      "Sistema desenvolvido com Java(Spring Boot) no back-end, com disparo de mensagens via Twilio e front-end desenvolvido em React.",
+    status: "Completo",
+  },
+  {
+    id: "sisrepo",
+    name: "SisRepo",
+    type: "Code",
+    link: "https://github.com/Romulo-Costa-De-Oliveira/SIS-REPO",
+    techicons: [i[9], i[5], i[4], i[6], i[1], i[7]],
+    descripition:
+      "Cadastro de repositórios do GitHub separados por usuário. Front-End desenvolvido utilizando a biblioteca React.Js do Javascript e Back-End construído em Node.Js e MongoDB como database.",
+    status: "Implementando o AuthContext...",
+  },
+  {
+    id: "siscrm",
+    name: "SisCrm",
+    type: "Code",
+    link: "https://github.com/Romulo-Costa-De-Oliveira/SIS-CRM",
+    techicons: [i[9], i[5], i[4], i[6], i[3]],
+    descripition:
+      "Front-end desenvolvido em React.Js para cadastro gerenciamento de usuários fazendo bom uso dos Hooks e utilizando Authentication e FireStore Database do Firebase.",
+    status: "Completo",
+  },
+  {
+    id: "dsmovie",
+    name: "DsMovie",
+    link: "https://github.com/Romulo-Costa-De-Oliveira/dsmovie",
+    type: "Link",
+    techicons: [i[9], i[5], i[4], i[6], i[8], i[0]],
+    descripition:
+      "Site para avaliação de filmes desenvolvido em React.js e API em Java(Spring Boot) com cadastro de avaliações em banco de dados.",
+    status: "Completo",
+  },
+];
+export default ProjectList;
